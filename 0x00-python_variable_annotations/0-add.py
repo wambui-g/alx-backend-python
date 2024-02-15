baa8806a43148300833bcef78d4aaac1e69fd772
wambui-g <grayciekamau03@gmail.com>
2024-02-15 14:45:16 +0300

    0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,8 +1,18 @@
-#!/usr/bin/python3
-"""type annoted concat function"""
+#!/usr/bin/env python3
+"""
+Module with 'add' function. It takes two floats and returns their sum.
+"""
 
 
 def add(a: float, b: float) -> float:
-    """accepts a and b and returns float"""
-    result = a + b
-    return result
+    """
+    Function to add two floats.
+
+    Args:
+        a (float): First number.
+        b (float): Second number.
+
+    Returns:
+        float: Sum of a and b.
+    """
+    return a + b
